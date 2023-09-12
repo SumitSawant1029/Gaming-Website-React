2c84567e867b3805dd0baa7ffc9ce2bd1d294f04
Abhay Singh <abhaysingh14310@gmail.com>
2023-09-12 16:17:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
+
 import logo from './logo.svg';
 import './App.css';
 import Login from './component/Login';
 import Signin from './component/Signin';
 import Navbar from './component/Navbar';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
-  const [ui, setUi] = useState('Navbar');
-
-  const switchToSignin = (e) => {
-    setUi(e);
-  };
-
-  // Define a function to switch between components based on the 'ui' state
-  const renderUI = () => {
-    if (ui === 'Navbar') {
-      return <Navbar switchToSignin={switchToSignin} />;
-    }else if (ui === 'Signin'){
-      return <Signin switchToSignin={switchToSignin}/>;
-    }else if (ui === 'Login'){
-      return <Login switchToSignin={switchToSignin}/>;
-    }
-  };
-
   return (
-    <>
-      {renderUI()} {/* Call the function to render the selected component */}
-    </>
+    <Router>
+      <Container>
+        {/* Define your routes */}
+        <Routes>
+          {/* Add a route for the "/" path */}
+          <Route path="/" element={<Navbar />} />
+          <Route path="/Signup" element={<Signin />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
+
+const Container = styled.div``;
 
 export default App;
