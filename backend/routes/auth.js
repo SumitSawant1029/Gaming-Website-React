230774c53f4be8f740f77468e03409bbfa0e4fe9
sumitsawant1029 <sumitsawant1029@gmail.com>
2023-09-20 19:07:45 +0530

    Hashing Done + Salting

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,34 +2,36 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body ,validationResult } = require('express-validator')
-
-// Create a User Using : POST "/api/auth/" . Doesnt require Auth
+const bcrypt = require('bcrypt')
+// Create a User Using : POST "/api/auth/" . Doesn't require Auth
 router.post('/',
 [
     body('name','Enter a Valid Name').isLength({ min : 3 }),
     body('email','Enter a Valid Email').isEmail(),
-    body('password','Enter valid Password').isLength({ min:5 }),
+    body('password','Enter valid Password').isLength({ min: 5 }),
 ],async (req,res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()){
-        return res.status(400).json({error :errors.array() });
+        return res.status(400).json({error: errors.array() });
     }
     try{
         let user = await User.findOne({email: req.body.email});
         if(user){
-            return res.status(400).json({error: "Sorry A user with this already exist"})
+            return res.status(400).json({error: "Sorry, a user with this email already exists"})
         }
+        const salt = await bcrypt.genSalt(10);
+
+        const secpass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
-            name:req.body.name,
-            password:req.body.password,
-            email:req.body.email,
-    
+            name: req.body.name,
+            password: secpass,
+            email: req.body.email,
         })
-        res.json(user)
-    }catch(error){
+        res.json(user);
+    } catch(error) {
         console.error(error.message);
-        res.status(500).send("Some Error Occured")
+        res.status(500).send("Some Error Occurred");
     }
 })
 
-module.exports = router
+module.exports = router;
