16332e51fbe2d3e461a7c268a86ce67af59d33d4
sumitsawant1029 <sumitsawant1029@gmail.com>
2023-09-21 00:15:21 +0530

    Done with Token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require('../models/User');
 const { body ,validationResult } = require('express-validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const { Await } = require('react-router-dom');
+const JWT_SECRET = "GamingEc$mmerce"
 // Create a User Using : POST "/api/auth/" . Doesn't require Auth
 router.post('/',
 [
@@ -27,11 +30,54 @@ router.post('/',
             password: secpass,
             email: req.body.email,
         })
-        res.json(user);
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken = jwt.sign(data,JWT_SECRET)
+        res.json({authtoken});
     } catch(error) {
         console.error(error.message);
-        res.status(500).send("Some Error Occurred");
+        res.status(500).send("Internel Error Occurred");
     }
 })
+
+router.post('/checkuser',
+[
+   
+    body('email','Enter a Valid Email').isEmail(),
+    body('password','Enter valid Password').exists(),
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({error: errors.array() });
+    }
+
+    const {email,password}= req.body;
+    try {
+        let user = await User.findOne()
+        if(!user){
+            return res.status(400).json({error : "Please try Coreect crediantials"})
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password);
+
+        if(!passwordCompare){
+            return res.status(400).json({error : "Please try Coreect crediantials"})
+        }
+
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken = jwt.sign(data,JWT_SECRET)
+        res.json({authtoken});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internel Error Occurred");
+    }
+});
+
 
 module.exports = router;
