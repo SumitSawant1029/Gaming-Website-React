0cfbe2ac82307856898b399999f5c9166ef64ec8
sumitsawant1029 <sumitsawant1029@gmail.com>
2023-10-04 07:24:23 +0530

    Validation

--- src/component/Signin.js
+++ src/component/Signin.js
@@ -4,6 +4,8 @@ import { Link,useNavigate } from 'react-router-dom';
 
 function Signin() {
   const navigate = useNavigate();
+  const [showpassAlert, setShowpassAlert] = useState(false);
+  const [showmobalert, setshowmobalert] = useState(false);
   const [credentials1, setcredentials1] = useState({
 
     firstname: "",
@@ -13,41 +15,73 @@ function Signin() {
     DOB: "",
     email: "",
     password: "",
+    cpassword:"",
   });
+
 
   const Signuphandler = async (e) => {
     e.preventDefault();
-    console.log(credentials1);
-    const response = await fetch('http://localhost:5000/api/auth/createuser', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        firstname: credentials1.firstname,
-        lastname: credentials1.lastname,
-        gender: credentials1.gender,
-        mob: credentials1.mob,
-        DOB: credentials1.DOB,
-        email: credentials1.email,
-        password: credentials1.password
-      }),
-    });
-    const json = await response.json();
-    console.log(json);
-    if (json.success===true) {
-      // Redirect to the home page
-      navigate('/login');
-    }
+    const isPasswordMatch = credentials1.password === credentials1.cpassword;
+    const isMobileValid = credentials1.mob.length === 10;
+    const birthDate = new Date(credentials1.DOB);
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+    console.log(age);
+
     
+      if (isPasswordMatch && isMobileValid){
+        // const response = await fetch('http://localhost:5000/api/auth/createuser', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({
+        //     firstname: credentials1.firstname,
+        //     lastname: credentials1.lastname,
+        //     gender: credentials1.gender,
+        //     mob: credentials1.mob,
+        //     DOB: credentials1.DOB,
+        //     email: credentials1.email,
+        //     password: credentials1.password
+        //   }),
+        // });
+        // const json = await response.json();
+        // console.log(json);
+        // if (json.success===true) {
+        //   // Redirect to the home page
+        //   navigate('/login');
+        // }
+      }
+      else{
+        if (!isPasswordMatch) {
+          setShowpassAlert(true);
+        }
+        if (!isMobileValid) {
+          setshowmobalert(true);
+        }
+      }
+      
   };
 
   const onChange1 = (e) => {
     setcredentials1({ ...credentials1, [e.target.name]: e.target.value });
   };
+ 
+  
 
   return (
     <>
+      {showpassAlert && ( // Render the alert when showAlert is true
+        <div className='alert alert-warning' role='alert'>
+          Password Doesnt Match
+        </div>
+      )}
+      {showmobalert && ( // Render the alert when showAlert is true
+        <div className='alert alert-warning' role='alert'>
+          Please Enter an Correct Contact Number 
+        </div>
+      )}
+
       <div className='container my-5'>
         <div className='d-flex justify-content-center'>
           <form onSubmit={Signuphandler} className='rounded p-4' style={{ backgroundColor: '#fff', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)' }}>
@@ -94,7 +128,7 @@ function Signin() {
           </div>
           <div className="mb-3">
             <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-            <input type="password" onChange={onChange1} id="confirmPassword" className="form-control" />
+            <input type="password" name="cpassword" onChange={onChange1} id="confirmPassword" className="form-control" />
           </div>
 
           <button type="submit" onClick={Signuphandler} className="btn btn-primary">Sign Up</button>
